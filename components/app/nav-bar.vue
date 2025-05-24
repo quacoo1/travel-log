247bc4f8c42f6baf4e45ee91e01de8ffd48217fe
quacoo <quacoo.boat@gmail.com>
2025-05-24 20:49:13 +0000

    ✨ add landing page

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { UButton } from '#components'
+</script>
+
+<template>
+  <header class="flex h-16 shrink-0 items-center justify-between gap-2 border-b border-default px-4">
+    <UButton variant="ghost" icon="hugeicons:maps-location-02" color="neutral" to="/">
+      Travel Log
+    </UButton>
+    <UButton trailing-icon="hugeicons:github-01" size="lg">
+      Sign In
+    </UButton>
+  </header>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { UContainer } from '#components'
+</script>
+
+<template>
+  <UContainer>
+    <div class="relative isolated mt-8 py-20">
+      <div class="absolute bg-black/10 -z-10 inset-0" />
+      <div class="text-center">
+        <h1 class="text-5xl font-semibold tracking-tight text-balance sm:text-7xl">
+          Travel Log
+        </h1>
+        <p class="mt-8 max-w-2xl mx-auto text-pretty text-muted sm:text-lg">
+          Keep track of you travels and adventures. Add locations, photos, and notes to create a digital journal of you journeys.
+        </p>
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <UButton trailing-icon="hugeicons:github-01" size="xl">
+            Sign in with github
+          </UButton>
+        </div>
+      </div>
+    </div>
+  </UContainer>
+</template>
