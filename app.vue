1ec8307fea90bc33f073d5e4884083576a55110e
Kay <quacoo.boat@gmail.com>
2025-05-24 20:52:02 +0000

    ✨ add landing page (#52)

--- app.vue
+++ app.vue
@@ -2,12 +2,9 @@
 </script>
 
 <template>
-  <div>
-    <UApp>
-      <h1 class="text-2xl font-bold underline">
-        Hello from the tailwind
-      </h1>
-      <UIcon name="hugeicons:location-01" class="size-6" />
-    </UApp>
-  </div>
+  <UApp>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
 </template>
