25481e38db1ea755e81652017aa39476282ab393
Kay <quacoo.boat@gmail.com>
2025-05-25 08:34:18 +0000

    ✨ setup typesafe env (#54)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,28 @@
 import antfu from '@antfu/eslint-config'
+
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 // TODO: add tailwind eslint
 export default withNuxt(antfu({
+  type: 'app',
+  typescript: true,
+  formatters: true,
   ignores: ['.pnpm-store/**'],
+}, {
+  rules: {
+    'ts/no-redeclare': 'off',
+    'ts/consistent-type-definitions': ['error', 'type'],
+    'no-console': ['warn'],
+    'antfu/no-top-level-await': ['off'],
+    'node/prefer-global/process': ['off'],
+    'node/no-process-env': ['error'],
+    'perfectionist/sort-imports': ['error', {
+      tsconfigRootDir: '.',
+    }],
+    'unicorn/filename-case': ['error', {
+      case: 'kebabCase',
+      ignore: ['README.md'],
+    }],
+  },
 }))
