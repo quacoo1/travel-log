3212a8a84cf3a04e5c8bb9a991105cf3f0827cce
quacoo <quacoo.boat@gmail.com>
2025-06-05 22:33:09 +0000

    💄 Change colors, font and title text

--- app.vue
+++ app.vue
@@ -1,4 +1,26 @@
 <script setup lang="ts">
+useHead({
+  meta: [
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' },
+  ],
+
+  htmlAttrs: {
+    lang: 'en',
+  },
+})
+
+const title = 'Travel Log'
+const description = 'Simple travel log built with nuxt.'
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+})
 </script>
 
 <template>
