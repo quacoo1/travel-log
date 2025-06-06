c9dd0ad977ebb9e15a6ea40842c89dc44c043e88
Kay <quacoo.boat@gmail.com>
2025-06-06 10:55:42 +0000

    ✨ Add navigation route protection for dashboard routes (#61)

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const authStore = useAuthStore()
+const { user } = storeToRefs(authStore)
+</script>
+
 <template>
   <div class="relative isolated py-20 border border-accented border-dashed">
     <div class="absolute -z-10 inset-0" />
@@ -9,7 +14,10 @@
         Keep track of you travels and adventures. Add locations, photos, and notes to create a digital journal of you journeys.
       </p>
       <div class="mt-10 flex items-center justify-center gap-x-6">
-        <AuthButton size="xl" extended-label />
+        <UButton v-if="user" to="/dashboard" size="xl">
+          Start Logging
+        </UButton>
+        <AuthButton v-else size="xl" extended-label />
       </div>
     </div>
   </div>
